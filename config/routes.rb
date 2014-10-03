5ee712a0651d01922711a75f77492b380d6dcf71
z13052hn <z13052hn@aiit.ac.jp>
2014-10-03 16:02:24 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  namespace :staff do
+    root 'top#index'
+  end
+
+  namespace :admin do
+    root 'top#index'
+  end
+
+  namespace :customer do
+    root 'top#index'
+  end
+end
